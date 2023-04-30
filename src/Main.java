74f9ee094a71e87f618a35e0f54af9136ec40af0
Nurik <nalymbaide@gmail.com>
2023-04-30 21:21:21 +0600

    commit

---
+++ src/Main.java
@@ -0,0 +1,180 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }}
+
+//         Massivge attardy berip methodton at berip oksh bolso true je false dep chygaruu
+//
+//        String[] name = {"Nurik", "Kocha", "Nurbolot"};
+//        System.out.println(getName(name));
+//
+//    }
+//    public static boolean getName(String[] name) {
+//        boolean names = false;
+//        String nam = "nurik";
+//        for (int i = 0; i < name.length; i++) {
+//
+//
+//            if (name[i].equals(nam)) {
+//                names = true;
+//            }
+//        }
+//        return names;
+//    }
+//}
+//
+
+
+
+//     MASSIVDIN ICHINDEGI ENG CHON JANA EING KICHINE SANDY TABUU
+//
+//        int [] number = {2,4,1};
+//        getSum(number);
+//
+//          }
+//    public static void getSum(int [] number){
+//        int maxNumber = number[0];
+//        for (int i=0; i<number.length; i++) {
+//            if(number[i]<maxNumber){
+//                maxNumber=number[i];
+//
+//
+//
+//
+//
+//            }
+//
+//        }System.out.println("max:"+maxNumber);
+//    }}
+//
+
+
+
+//       MASSIVDEGI SANDARGA 1 KOSHUP CNYGUU
+//
+//
+//        int[] number = {12, 20, 30, 45, 50, 200};
+//        int [] result = getSum(number);
+//        System.out.println(Arrays.toString(result));
+//
+//    }
+//
+//    public static int[] getSum(int[] number) {
+//        int[] mas = new int[number.length];
+//        for (int i = 0; i < number.length; i++) {
+//            mas[i] = number[i] + 1;
+//
+//
+//        }
+//        return mas;
+//
+//    }
+//}
+//
+//
+
+
+//              METHOD KOLDONUP SOZDU TESKERISINEN CHYGARUU
+//
+//
+//        System.out.println(reverseString("Nurik"));
+//
+//
+//    }
+//    public static String reverseString(String name){
+//        String result = "";
+//        for (int i = 0; i <name.length() ; i++) {
+//            result = name.charAt(i)+result;
+//
+//        }return result;
+//    }
+//
+//
+//}
+
+// 4 degen san bersek "Nurik" jolu chygaryp beruu
+
+//    Scanner scanner = new Scanner(System.in);
+//getNumber(4, "Nurik");
+//    }
+//    public static void getNumber(int number,String name){
+//
+//        for (int i = 0; i <number ; i++) {
+//            System.out.println(name);
+//        }
+//        }
+//
+//}
+//
+
+
+    //   Method arkyluu on san je ters san ekenin tabuu
+
+    // Параметирине бир  санды алган жана ал сан
+    //оң болсо "Оң сан", терс болсо "Терс сан"   деп
+    //консолго  чыгаруучу метод жазыныз.
+    //Эскертуу биринчи кайтарбоочу(void) метод менен
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (true) {
+//            getSum(scanner.nextInt());
+//        }
+//
+//    }
+//
+//    public static void getSum(int number) {
+//
+//            if (number < 0) {
+//                System.out.println("ters san");
+//            } else if (number > 0) {
+//                System.out.println("on san ");
+//            }
+//
+//
+//    }
+
+
+    // Параметирине бирдей узундуктагы, бүтүн сандардын эки
+    //массивин алган жана эки массивдердин элементтеринин
+    //суммасын консолдон көрсөткөн метод жазыныз. Эгерде
+    //эки массивдин узундугу бирдей болбосо.  Анда консолдон
+    //"Массивдердин узундугу бирдей эмес!" деген соз чыксын.
+
+//
+//        int[] number =  {5,5,5,4,5};
+//        int[] number2 = {1,2,3,4,5};
+//        getSum(number, number2);
+//
+//    }
+//
+//
+//    public static void getSum(int[] number, int[] number2) {
+//        if (number.length==number2.length){
+//            System.out.println("barabar");
+//        }else{
+//            System.out.println("barar emes");
+//        }
+//        int sum = 0;
+//        for (int i = 1; i < number.length; i++) {
+//            sum = sum + number[i];
+//        }
+//        System.out.println(sum);
+//        int sum2 = 0;
+//        for (int j = 0; j < number2.length; j++) {
+//            sum2 = sum2 + number2[j];
+//
+//        }
+//        System.out.println(sum2);
+//
+//
+//    }}
+//
+//
+
+
+
